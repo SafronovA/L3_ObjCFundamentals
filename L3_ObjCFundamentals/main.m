7254f5151368cd970a19248d74b2f7e734d59a32
asafrona <alexey.safronov96@gmail.com>
2021-05-05 20:16:40 +0300

    Initial Commit

---
+++ L3_ObjCFundamentals/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  L3_ObjCFundamentals
+//
+//  Created by Aliaksei Safronau EPAM on 5.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
